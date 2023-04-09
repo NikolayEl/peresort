696400d959749261065f7a4d473a0a985b2fb679
Nikolay Elshin <nikolay19830223@gmail.com>
2023-04-09 21:57:15 +0800

    stop error

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,9 +1,6 @@
 ﻿#include "stdafx.h"
 #include "constans.h"
-
-template <typename T> void Print(const T arr[], const int n);
-template <typename T> void Print(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
+#include "Print.h"
 
 template <typename T> void FillRand(T arr[], const int n);
 void FillRand(double arr[], const int n);
@@ -506,25 +503,4 @@ void UniqueRand(double arr[ROWS][COLS], const int ROWS, const int COLS)
 
 		}
 	}
-}
-
-template <typename T> void Print(const T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-}
-template <typename T> void Print(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
 }
